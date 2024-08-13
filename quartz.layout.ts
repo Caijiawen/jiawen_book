676aa52aafcda3010e2a6324981408a21349f064
蔡嘉文 <caijiawen@MacBook-Pro-10.local>
2024-08-13 19:28:41 +0800

    Quartz sync: Aug 13, 2024, 7:28 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,8 +25,8 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
+    Component.DesktopOnly(Component.Search()),
+    Component.DesktopOnly(Component.Darkmode()),
     // Component.DesktopOnly(Component.Explorer()),
     Component.Explorer(),
   ],
